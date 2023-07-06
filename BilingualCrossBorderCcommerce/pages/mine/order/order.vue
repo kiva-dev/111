f6c9dbff9b0ba32172d9a35f3301a19da165193a
Leon <2764667439@qq.com>
2023-07-06 20:07:18 +0800

    赠加跳转

--- BilingualCrossBorderCcommerce/pages/mine/order/order.vue
+++ BilingualCrossBorderCcommerce/pages/mine/order/order.vue
@@ -2,9 +2,11 @@
 	<view class="order-page">
 		<uni-header :title="$t('top.MyOrder')"></uni-header>
 		<view class="tab-layout">
-			<scroll-view class="tab-layout-scroll" scroll-x="true" :scroll-left="scrollLeft">
-				<view class="scroll-item" :class="{'scroll-active' : tabIndex === item.id}" v-for="item in tabArr" :key="item.id" @click="onChangeTab(item.id)">
-					<text :style="{'color': tabIndex === item.id ? 'rgb(10, 198, 142)' : 'rgb(51, 51, 51)'}">{{item.name}}</text>
+			<scroll-view class="tab-layout-scroll" scroll-x="true" :scroll-left="scrollLeft" @scroll="scroll">
+				<view class="scroll-item" :class="{'scroll-active' : tabIndex === item.id}" v-for="item in tabArr"
+					:key="item.id" @click="onChangeTab(item.id)">
+					<text
+						:style="{'color': tabIndex === item.id ? 'rgb(10, 198, 142)' : 'rgb(51, 51, 51)'}">{{item.name}}</text>
 				</view>
 			</scroll-view>
 		</view>
@@ -17,15 +19,21 @@
 							<p>{{item.shop_name}}</p>
 						</view>
 						<view class="header-status" style="color: #FF3939;" v-if="item.status === '0'">
-							{{$t('user.order.paid')}}</view>
+							{{$t('user.order.paid')}}
+						</view>
 						<view class="header-status" style="color: #1DD181;" v-if="item.status === '2'">
-							{{$t('user.order.shipped')}}</view>
+							{{$t('user.order.shipped')}}
+						</view>
 						<view class="header-status" style="color: #1DD181;" v-if="item.status === '3'">
-							{{$t('user.order.received')}}</view>
+							{{$t('user.order.received')}}
+						</view>
 						<view class="header-status" style="color: #999999;" v-if="item.status === '5'">
-							{{$t('user.order.confirmed')}}</view>
-						<view class="header-status" style="color: #999999;" v-if="item.status === '6' || item.status === '-1' || item.status === '-2'">
-							{{$t('user.order.closed')}}</view>
+							{{$t('user.order.confirmed')}}
+						</view>
+						<view class="header-status" style="color: #999999;"
+							v-if="item.status === '6' || item.status === '-1' || item.status === '-2'">
+							{{$t('user.order.closed')}}
+						</view>
 					</view>
 					<view class="ll-item-info" v-for="goods in item.goods" :key="goods.goods_id">
 						<view class="info-container">
@@ -47,14 +55,16 @@
 					<view class="ll-item-btns">
 						<template v-if="item.status === '0'">
 							<view class="btns-grey" @click.stop="onCancelOrder(item)">{{$t('user.order.cancel')}}</view>
-							<view class="btns-green" @click.stop="toOrderInfo(item.order_no)">{{$t('user.order.payment')}}</view>
+							<view class="btns-green" @click.stop="toOrderInfo(item.order_no)">
+								{{$t('user.order.payment')}}</view>
 						</template>
 						<template v-if="item.status === '2'">
 							<view class="btns-grey" @click.stop="onCancelOrder(item)">{{$t('user.order.cancel')}}</view>
 						</template>
 						<template v-if="item.status === '3'">
 							<!-- <view class="btns-grey" @click.stop="onAfterSale(item)">{{$t('user.order.sale')}}</view> -->
-							<view class="btns-green" @click.stop="onConfirmReceipt(item)">{{$t('user.order.receipt')}}</view>
+							<view class="btns-green" @click.stop="onConfirmReceipt(item)">{{$t('user.order.receipt')}}
+							</view>
 						</template>
 						<!-- <template v-if="item.status === '5'">
 							<view class="btns-grey" @click.stop="onAfterSale(item)">{{$t('user.order.sale')}}</view>
@@ -102,36 +112,43 @@
 				page: 1,
 				orderList: [],
 				scrollLeft: 0,
-				sendTit:10
+				sendTit: 10
 			}
 		},
 		onLoad(option) {
 			this.tabIndex = parseInt(option.tabIndex);
 			this.sendTit = parseInt(option.tabIndex);
 			this.getOrderList();
+			setTimeout(() => {
+				if (this.tabIndex === 5 || this.tabIndex === 6) {
+					console.log(this.tabIndex)
+					this.scrollLeft = 500;
+				} else {
+					this.scrollLeft = 0;
+				}
+			},50)
 		},
 		onShow() {
-			if (this.tabIndex === 5 || this.tabIndex === 6) {
-				this.scrollLeft = 500;
-			} else {
-				this.scrollLeft = 0;
-			}
+
 		},
 		onReachBottom() {
 			this.page++;
 			this.getOrderList();
 		},
 		methods: {
-			toOrderInfo(order_no){
+			scroll(e) {
+				this.scrollLeft = e.detail.scrollLeft
+			},
+			toOrderInfo(order_no) {
 				uni.navigateTo({
-					url:'/pages/cart/orderinfo?order_no='+order_no
+					url: '/pages/cart/orderinfo?order_no=' + order_no
 				})
 			},
 			onBack() {
 				uni.navigateBack();
 			},
 			onChangeTab(id) {
-				if(id==6) this.sendTit = [-1,-2,6].toString()
+				if (id == 6) this.sendTit = [-1, -2, 6].toString()
 				else this.sendTit = id
 				this.tabIndex = id
 				this.page = 1;
@@ -214,7 +231,8 @@
 			// 申请售后
 			onAfterSale(item) {
 				uni.navigateTo({
-					url: '/pages/mine/order/sqsh?conter=' + JSON.stringify(item.goods[0]) + '&cent=' + JSON.stringify(item)
+					url: '/pages/mine/order/sqsh?conter=' + JSON.stringify(item.goods[0]) + '&cent=' + JSON
+						.stringify(item)
 				});
 			},
 		}
@@ -311,25 +329,25 @@
 
 				.ll-item-info {
 					width: 100%;
-					
+
 					.info-container {
 						width: 100%;
 						padding: 24rpx 0;
 						box-sizing: border-box;
 						display: flex;
 						align-items: center;
-						
+
 						.info-cover {
 							width: 160rpx;
 							height: 160rpx;
-						
+
 							image {
 								width: 100%;
 								height: 100%;
 								border-radius: 16rpx;
 							}
 						}
-						
+
 						.info-content {
 							margin-left: 24rpx;
 							flex: 1;
@@ -338,29 +356,29 @@
 							flex-direction: column;
 							justify-content: space-between;
 							align-items: flex-start;
-						
+
 							.info-content-name {
 								color: rgb(51, 51, 51);
 								font-size: 24rpx;
 								line-height: 36rpx;
 							}
-						
+
 							.info-content-num {
 								width: 100%;
 								display: flex;
 								justify-content: space-between;
 								align-items: center;
-						
+
 								.num-total {
 									color: rgb(153, 153, 153);
 									font-size: 20rpx;
 								}
-						
+
 								.num-price {
 									color: rgb(255, 57, 57);
 									font-size: 32rpx;
 									font-weight: bold;
-						
+
 									text {
 										font-size: 20rpx;
 									}
@@ -368,13 +386,13 @@
 							}
 						}
 					}
-					
+
 					.info-buts {
 						width: 100%;
 						display: flex;
 						justify-content: flex-end;
 						align-items: center;
-						
+
 						.info-btns-grey {
 							margin-left: 24rpx;
 							padding: 10rpx 20rpx;
