789d5cb39193e67038d640c05568ad419baede2f
HwHua <694332031@qq.com>
2023-07-14 19:03:56 +0800

    🐞 fix(session): 未登录 进入聊天模块

--- BilingualCrossBorderCcommerce/components/customerService/index.vue
+++ BilingualCrossBorderCcommerce/components/customerService/index.vue
@@ -2,6 +2,9 @@
   <view class="leftSider" :class="siderClasses">
     <view class="imgArr" v-show="imgShow">
       <image src="/static/images/mine/download.png" class="minImg" alt="" @click="toMessage('/pages/auction/download')">
+	  <view>
+      	<image src="/static/images/mine/aixin.png" class="minImg" alt="" @click="checkApp()">
+	  </view>
       <view class="people">
         <image src="/static/images/mine/p.png" class="pImg" alt="" @click="toMessage('/pages/auction/association')">
       </view>
@@ -10,15 +13,17 @@
     <image src="/static/images/new-index/addImg.png" class="newListImgDeg" v-if="imgShow" @click="toggleImage"></image>
     <image src="/static/images/new-index/addImg.png" class="newListImg" v-else @click="toggleImage" ></image>
   </view>
+
 </template>
 
 <script>
 export default {
   data() {
     return {
-      imgShow: true,
-      transformClass: false,
-      timer: null,
+		imgShow: true,
+		transformClass: false,
+		timer: null,
+		showContact: false,
     };
   },
   computed: {
@@ -27,7 +32,59 @@ export default {
     }
   },
   methods: {
+	checkApp() {
+		this.showContact = true;
+		this.$emit("showContactFun",this.showContact);
+	},
+	
+	isLogin() {
+		const userinfo = uni.getStorageSync('userinfo');
+		if (!userinfo || !userinfo.token) {
+			this.handleNotLoggedIn();
+		} else {
+			const token = userinfo.token.split('|');
+			const time = Math.floor(Date.now() / 1000);
+			if (parseInt(token[2]) - 2 < time) {
+				this.handleExpiredToken();
+			} else {
+				this.handleValidLogin(userinfo.token, userinfo.auth_token);
+			}
+		}
+	},
+	handleNotLoggedIn() {
+		const isEnglish = uni.getStorageSync('locale') === 'en';
+		const title = isEnglish ? 'Tips' : '温馨提示';
+		const content = isEnglish
+			? 'You have not logged in or your identity has expired, please log in.'
+			: '您暂未登录或身份过期，请前往登录。';
+		uni.showModal({
+			title: title,
+			content: content,
+			success: (res) => {
+				if (res.confirm) {
+					uni.redirectTo({
+						url: '/pages/public/login',
+					});
+				} else {
+					uni.navigateBack();
+				}
+			},
+		});
+	},
+	handleExpiredToken() {
+		setTimeout(() => {
+			this.ws.logout();
+		}, 300);
+	},
+	handleValidLogin(token, auth_token) {
+		this.ws.init(token, auth_token);
+	},
 	openSession: function() {
+		const userinfo = uni.getStorageSync('userinfo');
+		this.isLogin()
+		if (!userinfo || !userinfo.token) {
+			return;
+		}
 		this.ws.pageFun(() => {
 			this.ws.send({ c: 'Message', a: 'assignCsr', data: {} })
 		})
@@ -46,9 +103,14 @@ export default {
       this.imgShow = !this.imgShow;
     },
     toMessage(url) {
-      uni.navigateTo({
-        url
-      })
+		const userinfo = uni.getStorageSync('userinfo');
+		this.isLogin()
+		if (!userinfo || !userinfo.token) {
+			return;
+		}
+		uni.navigateTo({
+			url
+		})
     },
   },
 };
@@ -87,7 +149,7 @@ export default {
 	.imgArr {
 		background: #fff;
 		width: 92rpx;
-		height: 280rpx;
+		height: 380rpx;
 		border-radius: 45rpx;
 		box-shadow: 0 0 20rpx rgba(198, 198, 198, 0.3);
 		text-align: center;
