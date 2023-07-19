993f7fbad8fa5435e8fe418864045ef629aee8d4
HwHua <694332031@qq.com>
2023-07-19 19:19:16 +0800

    ✨ feat: 论坛

--- BilingualCrossBorderCcommerce/pages/mine/LuckySharing/luckyforum.vue
+++ BilingualCrossBorderCcommerce/pages/mine/LuckySharing/luckyforum.vue
@@ -12,16 +12,34 @@
 				</view>
 			</view>
 		</view>
-        <view class="u-demo-block">
+        <view class="u-demo-block" v-for="(nub) in 10" :key="nub">
             <view class="u-demo-block__content">
                 <view class="album">
-                    <view class="album__avatar">
-                        <image src="/static/uview/common/logo.png" mode="" style="width: 32px;height: 32px;"></image>
+                    <view class="album__header">
+                        <view class="album__right">
+                            <view class="album__avatar">
+                                <u-avatar :src="src" size='40'></u-avatar>
+                                <!-- <image src="https://cdn.uviewui.com/uview/album/1.jpg" mode="widthFix" ></image> -->
+                            </view>
+                            <view>
+                                <u--text text="Eason Chan" bold size="17" class="album__text aaaaaaaaaaa"></u--text>
+                                <view class="item-l-level">
+                                    <view class="level-icon">
+                                        <image src="@/static/images/mine/mine_icon_vip.png" mode="widthFix"></image>
+                                    </view>
+                                    <view class="level-num">Lv.2</view>
+                                </view>
+                            </view> 
+                        </view>
+                        <view class="album__left">
+                            <view class="level-num">7-14</view>
+                        </view>
                     </view>
                     <view class="album__content">
-                        <u--text text="Eason Chan" bold size="17"></u--text>
                         <u--text margin="0 0 8px 0" :text="demoText" ></u--text>
-                        <u-album :urls="urls1" keyName="src2"></u-album>
+                        <view class="album__urls">
+                            <image :src="item" v-for="(item,index) in urls1" :key="index"></image>
+                        </view>
                     </view>
                 </view>
             </view>
@@ -35,9 +53,14 @@
 			return {
                 albumWidth: 0,
                 demoText:'This is my first successful wish since using KOLIBRI. I am really happy to be able to participate in the wish at such a low price and become the lucky one. My joy in sharing this is to promote this platform for more people to use and participate in the wish. Thank you.',
-                urls1: [{
-                    src2: 'https://cdn.uviewui.com/uview/album/1.jpg',
-                }]
+                src: 'https://cdn.uviewui.com/uview/album/1.jpg',
+                urls1: [
+                    'https://cdn.uviewui.com/uview/album/1.jpg',
+                    'https://cdn.uviewui.com/uview/album/2.jpg',
+                    'https://cdn.uviewui.com/uview/album/3.jpg',
+                    'https://cdn.uviewui.com/uview/album/4.jpg',
+                    'https://cdn.uviewui.com/uview/album/5.jpg'
+                ]
             }
 		},
         computed: {
@@ -65,16 +88,79 @@
 .album {
     display: flex;
     align-items: flex-start;
-    .album__avatar {
-            background-color: #0f0;
-            padding: 5px;
-            border-radius: 3px;
+    flex-direction: column;
+    padding: 40rpx;
+    .item-l-level{
+        width: 80rpx;
+        height: 30rpx;
+        margin-top: 8rpx;
+        padding-right: 5rpx;
+        box-sizing: border-box;
+        background: rgb(253, 240, 226);
+        border-radius: 100rpx;
+        display: flex;
+        .level-icon {
+            width: 30rpx;
+            height: 30rpx;
+            image {
+                width: 100%;
+            }
         }
-
+        .level-num {
+            margin-left: 5rpx;
+            color: rgb(219, 132, 37);
+            font-size: 16rpx;
+            line-height: 30rpx;
+        }
+    }
+    .album__header{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+        .album__right,
+        .album__left{
+            width: 50%;
+            display: flex;
+            align-items: center;
+        }
+        .album__right{
+            justify-content: flex-start;
+            .album__avatar {
+                margin-right: 40rpx;
+                image{
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+        }
+        .album__left{
+             justify-content: flex-end;
+             color: #666;
+             font-size: 16px;
+        }
+        .album__text{
+            margin: 20rpx 0;
+        }
+    }
     .album__content {
-            margin-left: 10px;
-            flex: 1;
+        margin:45rpx 0rpx;
+        flex: 1;
+        .album__urls{
+            margin-top:40rpx;
+            display: flex;
+            align-items: center;
+            flex-direction: row;
+            flex-wrap: wrap;
+            align-content: space-between;
+            image{
+                width: 180rpx;
+                height: 180rpx !important;
+                margin: 0 40rpx 40rpx 0;
+                border-radius: 24rpx;
+            }
         }
+    }
 }
 .mp-header {
     width: 100%;
