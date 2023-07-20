7cec7c4da96cd44fd4c4fb730130dc52ee601203
HwHua <694332031@qq.com>
2023-07-20 13:58:22 +0800

    ✨ feat: 幸运分享

---
+++ BilingualCrossBorderCcommerce/components/session/input.vue
@@ -0,0 +1,59 @@
+<template>
+    <view>
+        <view class="input-box u-f-ac u-f-jsa">
+        <input type="text" confirm-type="发送" v-model="val" placeholder="说点什么吧" @confirm="send" :focus="commit"/>
+        <view class="view-btn">
+                <button type="primary" @tap="send">发送</button>
+            </view>
+        </view>
+    </view>
+</template>
+
+<script>
+	export default {
+		name: "session",
+		data() {
+			return {
+
+			};
+		},
+        props:['commit'],
+		methods:{
+			send(){
+				this.$emit("submit",this.val)
+				this.val = '';
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+.input-box{
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 108rpx;
+    box-sizing: border-box;
+    padding: 20rpx;
+    border-top: 1px solid rgba(204,204,204,.4);
+    background-color: #fff;
+    input{
+        // border-radius: 30rpx;
+        border: 1px solid #EEEEEE;
+        background: #EEEEEE;
+        padding-left: 20rpx;
+        padding-top: 10rpx;
+        padding-bottom: 10rpx;
+        flex: 1;
+        margin-right:20rpx;
+    }
+    .view-btn{
+        width: 80px;
+    }
+    button{
+        background-color:#FFC000;
+        font-size: 14px;
+    }
+}
+</style>
--- BilingualCrossBorderCcommerce/pages/mine/LuckySharing/luckyforum.vue
+++ BilingualCrossBorderCcommerce/pages/mine/LuckySharing/luckyforum.vue
@@ -19,7 +19,6 @@
                         <view class="album__right">
                             <view class="album__avatar">
                                 <u-avatar :src="src" size='40'></u-avatar>
-                                <!-- <image src="https://cdn.uviewui.com/uview/album/1.jpg" mode="widthFix" ></image> -->
                             </view>
                             <view>
                                 <u--text text="Eason Chan" bold size="17" class="album__text aaaaaaaaaaa"></u--text>
@@ -41,17 +40,42 @@
                             <image :src="item" v-for="(item,index) in urls1" :key="index"></image>
                         </view>
                     </view>
+                    <view class="album__bottom">
+                        <view class="album__bottom__btn">
+                            <view class="Like">
+                                <image src="@/static/images/mine/unLike.png" mode="widthFix" v-if="false"></image>
+                                <image src="@/static/images/mine/Like.png" mode="widthFix" v-else></image>
+                                <view class="Like-num">{{likeNub}}</view>
+                            </view>
+                            <view class="forumComment">
+                                <image src="@/static/images/mine/forumComment.png" mode="widthFix"></image>
+                                <view class="forumComment__comment" @click="tocommit('id')">Comment</view>
+                            </view>
+                        </view>
+                        <view class="album__bottom__text">
+                            <view>Jackie Chan: WOW! Is so beautiful !!! i like</view>
+                            <view>Eason : Can you share your user experienceser experience?</view>
+                            <view>Jackie Chan: WOW! Is so beautiful !!! i like</view>
+                        </view>
+                    </view>
                 </view>
             </view>
         </view>
+        <input-box :commit="commit"  @submit="submit"></input-box>
     </view>
 </template>
 
 <script>
+import inputbox from "@/components/session/input.vue";
 	export default {
+        components:{
+            inputbox
+        },
 		data() {
 			return {
+                commit:false,
                 albumWidth: 0,
+                likeNub:1,
                 demoText:'This is my first successful wish since using KOLIBRI. I am really happy to be able to participate in the wish at such a low price and become the lucky one. My joy in sharing this is to promote this platform for more people to use and participate in the wish. Thank you.',
                 src: 'https://cdn.uviewui.com/uview/album/1.jpg',
                 urls1: [
@@ -69,6 +93,33 @@
             }
         },
 		methods:{
+            //键盘弹起
+            tocommit(x){
+                //通过commit传给子子告诉子用户点击评论区要评论
+                this.commit=true;
+                //这里的dyid是请求接口的时候用来告诉接口评论或回复的是那一条
+                this.dyid=x;
+                console.log(this.dyid)
+            },
+            //评论
+            submit(val) {
+                console.log(val);
+                if(val!=''){
+                let data={dynamicId:this.dyid}
+                    //接口调用
+                    this.$ajax.post(data).then(res=>{
+                        if(res.data.success){
+                            console.log(res);
+                        //评论完之后再调用一次查看所有评论的接口更新页面评论数据
+                            this.getComment();
+                            }else{
+                                
+                            }
+                    })
+                }else{
+                    this.$util.tips("评论文字不能为空")
+                }
+            },
             onBack(){
                 const routeArr = getCurrentPages().map(i => i.route);
 				if (routeArr.length > 1) {
@@ -85,11 +136,16 @@
 </script>
 
 <style lang="less" scoped>
+.u-page{
+    background: #F8F8F8;
+}
 .album {
     display: flex;
     align-items: flex-start;
     flex-direction: column;
     padding: 40rpx;
+    margin-bottom: 20rpx;
+    background: #fff;
     .item-l-level{
         width: 80rpx;
         height: 30rpx;
@@ -144,7 +200,7 @@
         }
     }
     .album__content {
-        margin:45rpx 0rpx;
+        margin:45rpx 0rpx 0 0;
         flex: 1;
         .album__urls{
             margin-top:40rpx;
@@ -159,6 +215,48 @@
                 margin: 0 40rpx 40rpx 0;
                 border-radius: 24rpx;
             }
+        }
+    }
+    .album__bottom{
+        // height: 80rpx;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        color: #666;
+        font-size: 16px;
+        .album__bottom__btn{
+            display: flex;
+            width: 100%;
+            align-items: center;
+            .Like{
+                display: flex;
+                justify-content: space-around;
+                align-items: center;
+                width: 15%;
+            }
+            .forumComment{
+                display: flex;
+                align-items: center;
+                margin-left: 40rpx;
+                justify-content: space-around;
+                width: 35%;
+                .forumComment__comment{
+
+                }
+            }
+            image{
+                width: 60rpx;
+            }
+        }
+        .album__bottom__text{
+            box-sizing: border-box;
+            width: 100%;
+            font-size: 28rpx;
+            background: #F5F5F5;
+            margin-top: 20rpx;
+            padding: 40rpx;
+            border-radius: 30rpx;
         }
     }
 }
