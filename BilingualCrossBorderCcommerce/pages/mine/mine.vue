f4b80c3df06d3abc5f56635134b1e30f8580c612
Leon <2764667439@qq.com>
2023-07-20 15:25:33 +0800

    修改我的

--- BilingualCrossBorderCcommerce/pages/mine/mine.vue
+++ BilingualCrossBorderCcommerce/pages/mine/mine.vue
@@ -109,18 +109,76 @@
 				</view>
 			</view>
 		</view>
-		
+
 		<!--邀请返佣-->
 		<view class="ml-commission">
 			<view class="ml-commission-box" @click="navClick('/pages/mine/new/commission')">
 				<p>{{$t('new.yqfy')}}</p>
 			</view>
-			<!-- <view class="commission-info">
-				
-			</view> -->
 		</view>
-		
-		
+
+		<view class="commission-info">
+			<view class="head">
+				<view class="head-left">Invitation Rebate</view>
+				<view class="head-more">
+					<view>{{$t('home.detail.more')}}</view>
+					<image src="/static/images/products/right.png"></image>
+				</view>
+			</view>
+
+			<view class="commission-line"></view>
+
+			<scroll-view scroll-x style="width: 686rpx;">
+				<view class="commission-switch">
+					
+					<view class="commission-left">
+						<view class="left-info">
+							<view class="left-tit-info">
+								<image src="/static/images/mine/yonjin.png"></image>
+								<view>Rebate amount(RM)</view>
+							</view>
+							<view class="left-info-num">1000.00</view>
+						</view>
+
+						<view class="left-info">
+							<view class="left-tit-info">
+								<view>My number of invitations</view>
+							</view>
+							<view class="left-info-num" style="font-size: 32rpx;">18</view>
+						</view>
+					</view>
+					
+					<view class="commission-right">
+						<view class="tit">
+							<image src="/static/images/mine/share-link.png"></image>
+							<view>Share Link</view>
+						</view>
+					
+						<view class="link-info">
+							<view class="info-key">Invitation Link:</view>
+							<view class="info-des">https://kjtest.ysxrj.cn/pages/mine/new/new-register?invite_code=693630
+							</view>
+							<image src="/static/images/mine/k_copy.png"></image>
+						</view>
+					
+						<view class="link-info">
+							<view class="info-key">Invitation Code:</view>
+							<view class="info-des">6936300</view>
+							<image src="/static/images/mine/k_copy.png"></image>
+						</view>
+					</view>
+					
+				</view>
+
+			</scroll-view>
+			
+			<view class="switch-info">
+				<text class="switch-info-name"></text>
+			</view>
+			
+		</view>
+
+
 		<view class="ml-auction">
 			<view class="ml-auction-top">
 				<view class="top-name">{{$t('tab.MymakeAwish')}}</view>
@@ -357,6 +415,7 @@
 				collectGoodsTotal: 0,
 				collectStoreTotal: 0,
 				totalJf: 0, //总积分
+				showLeftOrRight:false
 			}
 		},
 		onLoad() {
@@ -602,6 +661,169 @@
 </script>
 
 <style lang="less" scoped>
+	//返佣
+	.commission-info {
+		width: 686rpx;
+		padding: 24rpx 0;
+		background: #FFF;
+		border-radius: 20rpx;
+		margin: 24rpx auto;
+		
+		.commission-switch{
+			width: 1372rpx;
+			display: flex;
+			align-items: center;
+		}
+
+		.head {
+			position: relative;
+			width: 100%;
+			display: flex;
+			align-items: center;
+
+			.head-left {
+				font-size: 28rpx;
+				font-weight: bold;
+				color: rgb(51, 51, 51);
+				margin-left: 32rpx;
+			}
+
+			.head-more {
+				position: absolute;
+				right: 32rpx;
+				font-size: 24rpx;
+				color: rgb(51, 51, 51);
+				display: flex;
+				align-items: center;
+
+				image {
+					display: block;
+					width: 24rpx;
+					height: 24rpx;
+					margin-left: 8rpx;
+				}
+
+			}
+
+		}
+
+		.commission-line {
+			width: 622rpx;
+			border-bottom: 2rpx solid rgb(204, 204, 204);
+			margin: 24rpx auto;
+		}
+
+		.commission-left {
+			width: 686rpx;
+			display: flex;
+			align-items: center;
+
+			.left-info {
+				width: 50%;
+
+				.left-tit-info {
+					width: 100%;
+					font-size: 24rpx;
+					font-weight: bold;
+					color: rgb(51, 51, 51);
+					display: flex;
+					align-items: center;
+					justify-content: center;
+
+					image {
+						display: block;
+						width: 32rpx;
+						height: 32rpx;
+						margin-right: 10rpx;
+					}
+				}
+
+				.left-info-num {
+					width: 100%;
+					height: 162rpx;
+					font-size: 44rpx;
+					font-weight: bold;
+					color: rgb(51, 51, 51);
+					text-indent: 32rpx;
+					display: flex;
+					align-items: center;
+				}
+
+			}
+
+		}
+
+		.commission-right {
+			width: 686rpx;
+			// height: 194rpx;
+
+			.tit {
+				display: flex;
+				align-items: center;
+
+				image {
+					display: block;
+					width: 32rpx;
+					height: 32rpx;
+					margin-left: 32rpx;
+				}
+
+				view {
+					font-size: 24rpx;
+					font-weight: bold;
+					color: rgb(51, 51, 51);
+					margin-left: 16rpx;
+				}
+			}
+
+			.link-info {
+				width: 686rpx;
+				display: flex;
+				align-items: flex-start;
+				margin-top: 24rpx;
+
+				.info-key {
+					font-size: 24rpx;
+					color: rgb(51, 51, 51);
+					margin-left: 32rpx;
+				}
+
+				.info-des {
+					max-width: 386rpx;
+					font-size: 24rpx;
+					color: rgb(27, 161, 255);
+					word-break: break-all;
+					margin: 0 8rpx;
+				}
+
+				image {
+					width: 32rpx;
+					height: 32rpx;
+				}
+
+			}
+
+		}
+		
+		.switch-info{
+			position: relative;
+			width: 32rpx;
+			height: 6rpx;
+			background: rgb(232, 232, 232);
+			border-radius: 6rpx;
+			margin: 20rpx auto 0 auto;
+			
+			.switch-info-name{
+				position: absolute;
+				left: 0;
+				width: 60%;
+				height: 6rpx;
+				background: rgb(10, 198, 142);
+			}
+		}
+		
+	}
+
 	//右侧固定栏滚动
 	.removeRightX {
 		transform: translateX(80rpx);
