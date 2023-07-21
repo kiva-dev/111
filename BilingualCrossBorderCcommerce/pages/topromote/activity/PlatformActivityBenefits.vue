0ba0b06563b67a1c422dc5636491093d4c95f915
HwHua <694332031@qq.com>
2023-07-21 16:00:11 +0800

    ✨ feat: 分享跳转

--- BilingualCrossBorderCcommerce/pages/topromote/activity/PlatformActivityBenefits.vue
+++ BilingualCrossBorderCcommerce/pages/topromote/activity/PlatformActivityBenefits.vue
@@ -1,30 +1,12 @@
 <template>
 	<view class="address-page">
-	    <view  v-if="!true">
-            <image class="topImage" src="@/static/demo/top.png"></image>
-            <view class='topText'>平台活动福利</view>
-            <view class='topText-child'>超多惊喜</view>
-            <view class="commissionImage">
-                <image class="child" src="@/static/demo/commissionrefund.png"></image>
-                <image class="RedEnvelopes" src="@/static/demo/RedEnvelopesSurge.gif"></image>
-            </view>
-            <image class="shopImage" src="@/static/demo/shop.png"></image>
-            <image class="contentImage" src="@/static/demo/content-cn.png"></image>
-            <view class="fingerI">
-                <image class="fingerImage" src="@/static/demo/fingerdown.gif"></image>
-            </view>
-            <view class="bottomBtn">
-                <view style="position: relative;">
-                    <image class="contentImage" src="@/static/demo/DownloadBtn.png"></image>
-                    <view class="download">下载 APP</view>
-                </view>
-                <view style="position: relative;">
-                    <image class="contentImage" src="@/static/demo/Register.png"></image>
-                    <view class="Register">注册</view>
-                </view>
-            </view>
-        </view>
-        <view v-else>
+	    <view  v-if="isEnglish">
+            <view class="head-info">
+				<view class="top">
+					<image src="/static/images/surprise/return.png" class="left" @click="toBack()"></image>
+					<image src="/static/images/surprise/home.png" class="index" @click="toIndex()"></image>
+				</view>
+			</view>
             <image class="topImage" src="@/static/demo/top.png"></image>
             <view class='topText-en'>Platform Activity Benefits</view>
             <view class='topText-child-en'>Too many surprises</view>
@@ -32,7 +14,7 @@
                 <image class="child" src="@/static/demo/commissionrefund-en.png"></image>
                 <image class="RedEnvelopes" src="@/static/demo/RedEnvelopesSurge.gif"></image>
             </view>
-            <image class="shopImage" src="@/static/demo/shop-en.png"></image>
+            <image class="shopImage" src="@/static/demo/shop-en.png" @click="toIndex()"></image>
             <image class="contentImage" src="@/static/demo/content-en.png"></image>
             <view class="fingerI">
                 <image class="fingerImage" src="@/static/demo/fingerdown.gif"></image>
@@ -40,11 +22,41 @@
             <view class="bottomBtn">
                 <view style="position: relative;">
                     <image class="contentImage" src="@/static/demo/DownloadBtn.png"></image>
-                    <view class="download">Download app</view>
+                    <view class="download" @click="navClick('add')">Download app</view>
                 </view>
                 <view style="position: relative;">
                     <image class="contentImage" src="@/static/demo/Register.png"></image>
-                    <view class="Register">Register</view>
+                    <view class="Register" @click="navClick('/pages/public/login')">Register</view>
+                </view>
+            </view>
+        </view>
+        <view v-else>
+            <view class="head-info">
+				<view class="top">
+					<image src="/static/images/surprise/return.png" class="left" @click="toBack()"></image>
+					<image src="/static/images/surprise/home.png" class="index" @click="toIndex()"></image>
+				</view>
+			</view>
+            <image class="topImage" src="@/static/demo/top.png"></image>
+            <view class='topText'>平台活动福利</view>
+            <view class='topText-child'>超多惊喜</view>
+            <view class="commissionImage">
+                <image class="child" src="@/static/demo/commissionrefund.png"></image>
+                <image class="RedEnvelopes" src="@/static/demo/RedEnvelopesSurge.gif"></image>
+            </view>
+            <image class="shopImage" src="@/static/demo/shop.png" @click="toIndex()"></image>
+            <image class="contentImage" src="@/static/demo/content-cn.png"></image>
+            <view class="fingerI">
+                <image class="fingerImage" src="@/static/demo/fingerdown.gif"></image>
+            </view>
+            <view class="bottomBtn">
+                <view style="position: relative;">
+                    <image class="contentImage" src="@/static/demo/DownloadBtn.png"></image>
+                    <view class="download" @click="navClick('add')">下载 APP</view>
+                </view>
+                <view style="position: relative;">
+                    <image class="contentImage" src="@/static/demo/Register.png"></image>
+                    <view class="Register" @click="navClick('/pages/public/login')">注册</view>
                 </view>
             </view>
         </view>
@@ -56,23 +68,66 @@
 		data() {
 			return {
 				addList: [],
+                isEnglish:true,
 				page: 1,
 			}
 		},
-		onShow() {
+		onLoad(e) {
+			if(e.language == 'en') this.isEnglish = true
+			else this.isEnglish = false
+			
+			if(e.promotion_code) uni.setStorageSync('invite_code',e.promotion_code)
 		},
 		onReachBottom() {
 		},
 		methods: {
+            toBack() {
+				uni.navigateBack()
+			},
 			navClick(url) {
 				uni.navigateTo({
 					url
 				})
 			},
+			toIndex() {
+				uni.switchTab({
+					url: '/pages/auction/auction'
+				})
+			}
 		},
 	}
 </script>
 <style lang="less" scoped>
+    .head-info {
+        padding-top: 88rpx;
+        background-size: 750rpx 900rpx;
+        position: absolute;
+        z-index: 999;
+        width: 100%;
+        .top {
+            position: relative;
+            width: 100%;
+            height: 88rpx;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            image {
+                display: block;
+                width: 40rpx;
+                height: 40rpx;
+            }
+
+            .left {
+                margin-left: 32rpx;
+            }
+
+            .index {
+                position: absolute;
+                right: 32rpx;
+            }
+        }
+
+    }
 	.address-page {
 		width: 100%;
 		min-height: 200vh;
