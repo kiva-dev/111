61926b13bbdb1beb46625f7b15b71976af58a006
HwHua <694332031@qq.com>
2023-08-23 16:41:49 +0800

    🌈 style(K币)

--- BilingualCrossBorderCcommerce/pages/mine/K_coin_introduction.vue
+++ BilingualCrossBorderCcommerce/pages/mine/K_coin_introduction.vue
@@ -5,8 +5,8 @@
 			<view>{{$t('kIntroduction.title')}}</view>
 		</view>
 
-		<view class="list">
-            <view v-for="(item,i) in list" class="item" :style="(i+1==list.length)?'border-bottom: none;':''">
+		<view class="listClass">
+            <view v-for="(item,i) in list" class="item">
                 <view class="content">
                     {{isShopCont ? item.des : item.des_cn}}
                 </view>
@@ -22,52 +22,24 @@
 			return {
 				isShopCont: uni.getStorageSync('locale') == 'en' ? true : false,
 				list: [{
-						des: `You can sign up directly via the invitation link or you may click"Sign Up"/"Register now" using your email address or mobile number and wait for the verification code to be sent to either one.`,
-						des_cn: `您可以通过邀请链接直接注册，输入您可以使用您的电子邮件地址或手机号码点击"登陆" / "注册"，并等待验证码发送到其中一个。`
+						des: `Kolibri Mall Coin (KMC) is a cryptocurrency issued by the official Kolibri project in collaboration with globally recognized compliant cryptocurrency exchanges, aiming to welcome the Web3.0 era and reward platform users. All Kolibri users can exchange and receive KMC Coin for free.`,
+						des_cn: `Kolibri Mall Coin是Kolibri官方计划为迎接Web3.0时代、奖励回馈平台用户而联合全球合规知名加密货币交易所发行的加密货币，所有Kolibri用户可免费兑换领取。`
 					},
 					{
-						des: `No, you don’t have to. All registration to our e-commerce platform is free!`,
-						des_cn: `不需要，我们的电子商务平台所有注册都是免费的！`
+						des: `A total of 8 billion KMC tokens are planned for issuance. Among them, 30% are reserved for platform operations and investor feedback, 10% for public sales to external users, and 60% for rewarding users and partners. There are two ways to acquire KMC coin: Kolibri users can exchange them for free based on their accumulated platform's points, and external users can purchase them on exchanges after being listed on the blockchain. Therefore, please take good care of your account and accumulated points.`,
+						des_cn: `KMC币计划总共发行80亿枚，其中30%为平台保留用于运营及投回馈投资者，10%用于对外部用户公开销售、60%用来回馈奖励用户及合作伙伴。仅Kolibri用户根据积分免费兑换，以及上链后外部用户在交易所付费购买，两种获取方式。所以请务必珍惜您的账户，以及积分。`
 					},
 					{
-						des: `You can try to resend the verification code to your number or try to use your email address to register.`,
-						des_cn: `您可以尝试重新发送验证码到您的号码，或尝试使用您的电子邮件地址进行注册。`
+						des: `Currently, each verified user can exchange their accumulated platfrom‘s points in their Kolibri account for KMC coin at a 1:1 ratio (this ratio may be adjusted later based on demand). Each individual can only possess one unique verified account, and users and identities that have been banned or removed cannot receive coin.`,
+						des_cn: `目前，每个实名用户根据Kolibri账户中累积的共创积分免费1：1兑换（后续会根据需求进行下调），一个身份只能拥有一个唯一的实名账户，被封禁、除名的用户及身份无法领取。`
 					},
 					{
-						des: `You cannot participate if you are under 18 years old unless you have your guardian approval following the Policy Agreement during your signing up procedure. `,
-						des_cn: `如果您未满18岁，除非在注册过程中遵循政策协议获得监护人的批准，否则您不能参与。`
+						des: `Disclaimer: Kolibri does not provide cryptocurrency trading platforms or functions. Before being listed on a public blockchain, KMC trading is not available, and circulation trading is prohibited. Only points can be exchanged for KMC coin for bookkeeping purposes. After being listed on a public blockchain, Kolibri will distribute coin based on users' provided cryptocurrency wallet addresses. Users can freely trade and hold KMC coin on designated and legally regulated cryptocurrency exchanges in countries such as the United States, Singapore, South Korea, Japan, Dubai, Hong Kong, and Malaysia.`,
+						des_cn: `声明：Kolibri不提供加密货币交易平台及功能，未上公链之前，不开放KMC交易，禁止流通交易。仅提供积分兑换KMC记账。上公链后，Kolibri根据用户提供的加密钱包地址进行空投，用户可在美国、新加坡、韩国、日本、迪拜、香港、马来西亚等合法受监管的指定加密货币交易所自由交易、持有。`
 					},
 					{
-						des: `You can click on the Profile Picture>Account & Security>Real Name Authentication>Upload front and back IC photos.`,
-						des_cn: `您可以点击头像 > 个人认证 > 实名认证 > 上传身份证正反面照片。`
-					},
-					{
-						des: `We must do the Real Name Authentication as we need to avoid frauds from happening and our platform are bind with rules and regulations.`,
-						des_cn: `我们必须进行实名认证，以避免发生欺诈行为，我们的平台受到法规的约束。`
-					},
-					{
-						des: `You may click on your Profile Picture and choose Nickname to do modification or set a new one.`,
-						des_cn: `您可以点击您的个人资料图片，选择"昵称"进行修改或设置一个新的昵称。`
-					},
-					{
-						des: `Hi, you can go on the "Login page" and click on "forgot password" to reset your passwords.`,
-						des_cn: `您可以在"登录页面"上点击"忘记密码"以重置密码。`
-					},
-					{
-						des: `Go to Invitation Rebate>Join Now> you may copy the link or share your invite code with your friends.`,
-						des_cn: `前往"邀请返佣" > "我的邀请">> "立即邀请">您可以复制链接或与您的朋友分享邀请码。`
-					},
-					{
-						des: `Go to "Ongoing" tab and you can see the ongoing Wish items, just click on the Wish you want to participate and click on the "Wish" button and choose the amount of wish you desire and wait for the Lucky Star to be announced.`,
-						des_cn: `前往"正在进行"选项，您可以看到正在进行中的许愿活动，只需点击您想要参与的许愿活动，并点击"许愿"按钮，选择您想要的许愿金额，然后等待幸运之星的揭晓。`
-					},
-					{
-						des: `You may recharge in My Asset>K-Diamonds>Select the desire amount(minimum of RM15)`,
-						des_cn: `您可以在"我的资产"中进行充值，选择K-Diamonds，然后选择所需金额（最低RM15）。`
-					},
-					{
-						des: `For now,we only provide two payment methods which is either using PayPal or Payessence and it both requires your bank account. Soon to come another payment gateway!`,
-						des_cn: `目前，我们只提供两种付款方式，即PayPal和Payessence，这两种方式都需要银行账户。即将推出另一种付款网关！`
+						des: `We uphold and fulfill our commitment: to provide a platform that allows everyone to realize their dreams, to enable everyone to have desired goods, to increase value through consumption, and to gain additional value and wealth appreciation through our platform.`,
+						des_cn: `我们遵守并兑现承诺：为每个人提供可以实现梦想的平台，让每个人都能拥有心仪的商品，让用户能在消费中增值，获得额外的价值回报，财富增值！`
 					}
 				]
 			}
@@ -111,19 +83,17 @@
 			}
 		}
 
-		.list {
+		.listClass {
 			width: 750rpx;
-			padding: 40rpx 0;
 			background: #fff;
 			margin-top: 20rpx;
-
+			padding: 0 40rpx;
+    		box-sizing: border-box;
 			.item {
 				width: 100%;
 				padding: 40rpx 0;
 				display: flex;
 				align-items: flex-start;
-				border-bottom: 2rpx solid rgb(243, 243, 243);
-
 				.logo {
 					width: 56rpx;
 					height: 56rpx;
